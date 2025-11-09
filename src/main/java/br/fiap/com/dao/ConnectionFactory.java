69abd63e500da6ff22835f7d8902e313d4d19b08
Pedro Mariutti <pmariutti@gmail.com>
2025-11-09 11:08:00 -0300

    connectionfactory alteracao

--- src/main/java/br/fiap/com/dao/ConnectionFactory.java
+++ src/main/java/br/fiap/com/dao/ConnectionFactory.java
@@ -11,12 +11,18 @@ public class ConnectionFactory {
     public static Connection getConnection() {
         if (connection == null) {
             try {
-
                 Class.forName("oracle.jdbc.driver.OracleDriver");
 
-                String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-                String user = "rm75999";
-                String password = "150896";
+
+                String url = System.getenv("DB_URL");
+                String user = System.getenv("DB_USER");
+                String password = System.getenv("DB_PASSWORD");
+
+
+                if (url == null || user == null || password == null) {
+                    System.err.println("Variáveis de ambiente DB_URL, DB_USER, ou DB_PASSWORD não configuradas.");
+                    throw new RuntimeException("Variáveis de ambiente do banco não configuradas!");
+                }
 
 
                 connection = DriverManager.getConnection(url, user, password);
